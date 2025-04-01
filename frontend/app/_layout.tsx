23c9dec9e50f604a202fa8a487abd8af3fa682e6
lukasrzavsky <36069536+lukasrzavsky@users.noreply.github.com>
2025-04-01 15:27:58 +0200

    query provider with query client added to root layout

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -4,17 +4,20 @@ import {
 	DefaultTheme,
 	ThemeProvider,
 } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function RootLayout() {
 	const colorScheme = useColorScheme();
 	const [loaded] = useFonts({
 		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
 	});
+
+	const [queryClient] = useState(() => new QueryClient());
 
 	useEffect(() => {
 		if (loaded) {
@@ -30,10 +33,12 @@ export default function RootLayout() {
 		<ThemeProvider
 			value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
 		>
-			<Stack screenOptions={{ headerShown: false }}>
-				<Stack.Screen name="(tabs)" />
-			</Stack>
-			<StatusBar style="auto" />
+			<QueryClientProvider client={queryClient}>
+				<Stack screenOptions={{ headerShown: false }}>
+					<Stack.Screen name="(tabs)" />
+				</Stack>
+				<StatusBar style="auto" />
+			</QueryClientProvider>
 		</ThemeProvider>
 	);
 }
